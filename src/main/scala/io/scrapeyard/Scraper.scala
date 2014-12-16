5156b995d5476ede40499d00d7d91748a166fd35
frogue <zzmaric@gmail.com>
2014-12-16 23:24:11 +0100

    Selects dates and submits form.

--- src/main/scala/io/scrapeyard/Scraper.scala
+++ src/main/scala/io/scrapeyard/Scraper.scala
@@ -16,18 +16,31 @@ object Scraper extends Firefox {
     Thread.sleep(5000)
 
     click on "book"
-//    click on "book"
     Thread.sleep(2000)
+
     click on "FromTemp"
     Thread.sleep(2000)
-//    click on "bc_from1"
-//    enter("\t")
     enter("zagreb")
     Thread.sleep(2000)
     enter("\t")
+
     Thread.sleep(2000)
     enter("bali")
     Thread.sleep(2000)
     enter("\t")
+
+    click on "departing"
+    Thread.sleep(1000)
+    enter("21-May-2015")
+    Thread.sleep(1000)
+
+    click on "returning"
+    Thread.sleep(1000)
+    enter("31-Jul-2015")
+    Thread.sleep(1000)
+
+    click on "bookFlight"
+
+//    Thread.sleep(10000)
   }
 }
