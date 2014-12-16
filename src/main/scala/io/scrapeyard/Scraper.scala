474fbcfaf9376c868b0f45a80f639809500233b9
frogue <zzmaric@gmail.com>
2014-12-16 23:03:04 +0100

    Opens qatar airways and enters zagreb and bali.

--- src/main/scala/io/scrapeyard/Scraper.scala
+++ src/main/scala/io/scrapeyard/Scraper.scala
@@ -1,13 +1,33 @@
 package io.scrapeyard
 
 import org.scalatest.selenium.Firefox
+import org.scalatest.time.{Seconds, Span}
 
 object Scraper extends Firefox {
 
-  val host = "http://lovenirs.com"
+  val host = "http://www.qatarairways.com"
 
   def doIt: Unit = {
     go to host
     println(pageTitle)
+
+    implicitlyWait(Span(10, Seconds))
+
+    Thread.sleep(5000)
+
+    click on "book"
+//    click on "book"
+    Thread.sleep(2000)
+    click on "FromTemp"
+    Thread.sleep(2000)
+//    click on "bc_from1"
+//    enter("\t")
+    enter("zagreb")
+    Thread.sleep(2000)
+    enter("\t")
+    Thread.sleep(2000)
+    enter("bali")
+    Thread.sleep(2000)
+    enter("\t")
   }
 }
