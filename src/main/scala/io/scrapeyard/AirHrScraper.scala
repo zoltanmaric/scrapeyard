c322522e226d7fd6eaae8627bee8ae7e43a48a7c
frogue <zzmaric@gmail.com>
2014-12-27 19:55:21 +0100

    Added Air.Hr scraper.

---
+++ src/main/scala/io/scrapeyard/AirHrScraper.scala
@@ -0,0 +1,37 @@
+package io.scrapeyard
+
+import org.joda.time.format.DateTimeFormat
+import org.scalatest.Matchers
+import org.scalatest.concurrent.Eventually
+import org.scalatest.selenium.Firefox
+import org.scalatest.time.{Minutes, Span}
+
+import scala.concurrent.duration._
+// val ff = new FirefoxDriver with Firefox
+object AirHrScraper extends Firefox with Matchers with Eventually {
+
+  val host = "http://air.hr"
+
+  def doIt(ps: SearchParams): SearchResult = {
+    val fmt = DateTimeFormat.forPattern("dd.MM.yyyy")
+    val org = ps.origin
+    val dst = ps.destination
+    val dep = fmt.print(ps.departure)
+    val ret = fmt.print(ps.returning)
+    val query = s"http://avio.air.hr/airhr/$org/$dst/$dep-$ret/1/0/0/rt"
+    go to query
+    println(pageTitle)
+
+    implicitlyWait(Span(3, Minutes))
+
+
+    eventually (timeout(3 minutes)){
+      val p = find(cssSelector("div[class^=flight_price_v1] span[class=pull-right]")).get.text
+      assert(!p.replaceAll("\\s", "").isEmpty)
+    }
+
+    val price = find(cssSelector("div[class^=flight_price_v1] span[class=pull-right]")).get.text
+
+    SearchResult(ps, price, query)
+  }
+}
--- src/main/scala/io/scrapeyard/Main.scala
+++ src/main/scala/io/scrapeyard/Main.scala
@@ -3,39 +3,62 @@ package io.scrapeyard
 import org.joda.time.DateTime
 
 import scala.concurrent.{Await, Future}
-import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success, Try}
 import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object Main extends App {
   println("hello world")
 
   val origs = Set("ZAG", "BUD")
   val dests = Set("DPS")
-  val depFrom = DateTime.parse("2015-05-21T00:00:00.000Z")
+  val depFrom = DateTime.parse("2015-05-20T00:00:00.000Z")
   val depUntil = DateTime.parse("2015-05-22T00:00:00.000Z")
   val retFrom = DateTime.parse("2015-07-20T00:00:00.000Z")
   val retUntil = DateTime.parse("2015-07-31T00:00:00.000Z")
   val criteria = BatchSearchCriteria(origs, dests, depFrom, depUntil, retFrom, retUntil)
   val paramList = Dispatcher.toSearchParams(criteria)
 
-  private val qatarFuture = Future {
+//  private val qatarFuture = Future {
+//    paramList.foreach { ps =>
+//      Try(QatarScraper.doIt(ps)) match {
+//        case Success(r) => println(r)
+//        case Failure(t) => println((ps, t))
+//      }
+//    }
+//  }
+
+  private val airHrFuture = Future {
     paramList.foreach { ps =>
-      val res = QatarScraper.doIt(ps)
-      println(res)
+      Try(AirHrScraper.doIt(ps)) match {
+        case Success(r) => println(r)
+        case Failure(t) => println((ps, t))
+      }
     }
   }
-  qatarFuture.onComplete(println(_))
 
-  private val momondoFuture = Future {
-    paramList.foreach { ps =>
-      val res = MomondoScraper.doIt(ps)
-      println(res)
-    }
-  }
-  momondoFuture.onComplete(println(_))
+//  private val momondoFuture = Future {
+//    paramList.foreach { ps =>
+//      Try(MomondoScraper.doIt(ps)) match {
+//        case Success(r) => println(r)
+//        case Failure(t) => println((ps, t))
+//      }
+//    }
+//  }
+//  momondoFuture.onComplete(println(_))
 
-  Await.ready(qatarFuture, 1 hour)
-  Await.ready(momondoFuture, 1 hour)
+//  private val airHrFuture = Future {
+//    paramList.foreach { ps =>
+//      val res = Try(AirHrScraper.doIt(ps))
+//      println(res)
+//    }
+//  }
+//  airHrFuture.onComplete(println(_))
+
+//  Await.ready(qatarFuture, 1 hour)
+//  Await.ready(momondoFuture, 1 hour)
+  Await.ready(airHrFuture, 1 hour)
+
 
 //
 //  val arrivals = Seq(
