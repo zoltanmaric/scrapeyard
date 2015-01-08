29dc1d75b9a6502716f39b69c2684c28625e5d3e
frogue <zzmaric@gmail.com>
2015-01-08 23:11:42 +0100

    Removed obsolete commented code in Main.scala.

--- src/main/scala/io/scrapeyard/Main.scala
+++ src/main/scala/io/scrapeyard/Main.scala
@@ -8,7 +8,6 @@ import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 
 object Main extends App {
-  println("hello world")
 
   val origs = Set("ZAG", "BUD")
   val dests = Set("DPS")
@@ -58,36 +57,4 @@ object Main extends App {
 //  Await.ready(qatarFuture, 1 hour)
 //  Await.ready(momondoFuture, 1 hour)
   Await.ready(airHrFuture, 1 hour)
-
-
-//
-//  val arrivals = Seq(
-//    "25-Jul-2015",
-//    "26-Jul-2015",
-//    "27-Jul-2015",
-//    "28-Jul-2015",
-//    "29-Jul-2015",
-//    "30-Jul-2015",
-//    "31-Jul-2015",
-//    "01-Aug-2015",
-//    "02-Aug-2015",
-//    "03-Aug-2015",
-//    "04-Aug-2015",
-//    "05-Aug-2015",
-//    "06-Aug-2015",
-//    "07-Aug-2015",
-//    "08-Aug-2015",
-//    "09-Aug-2015",
-//    "10-Aug-2015"
-//  )
-//
-//  val arrivalsToPrices = arrivals.map { a =>
-//    val pair = (a, Scraper.doIt(a))
-//    println(pair)
-//    pair
-//  }.toMap
-//
-//  println(arrivalsToPrices.mkString("\n"))
-
-//  println(MomondoScraper.doIt(params))
 }
