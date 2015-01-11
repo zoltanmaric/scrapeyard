f8c6aad8b55b6a0a898249a39dfb1d8754c004aa
frogue <zzmaric@gmail.com>
2015-01-11 02:22:00 +0100

    Create server submodule

--- server/src/main/scala/io/scrapeyard/SilentHtmlUnit.scala
+++ server/src/main/scala/io/scrapeyard/SilentHtmlUnit.scala
@@ -7,5 +7,5 @@ import org.scalatest.selenium.HtmlUnit
 trait SilentHtmlUnit extends HtmlUnit {
   // HtmlUnit throws all kinds of error logs when an element has not yet appeared,
   // so we silence it. FORECFULLY! GGGAAAAAHH!!
-  java.util.logging.Logger.getLogger("com.gargoylesoftware.htmlunit").setLevel(Level.OFF);
+  java.util.logging.Logger.getLogger("com.gargoylesoftware.htmlunit").setLevel(Level.OFF)
 }
