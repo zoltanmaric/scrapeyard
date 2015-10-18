ab4b8ee12e070458d6900b03cf6e0ac9a9952e6f
frogue <zzmaric@gmail.com>
2015-10-18 16:06:33 +0200

    Momondo scraper test clean up

--- server/src/test/scala/io/scrapeyard/scrapers/MomondoSpec.scala
+++ server/src/test/scala/io/scrapeyard/scrapers/MomondoSpec.scala
@@ -1,12 +1,18 @@
 package io.scrapeyard.scrapers
 
 import io.scrapeyard.MomondoScraper
-import org.scalatest.{Matchers, WordSpecLike}
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 
-class MomondoSpec extends WordSpecLike with Matchers with TestParams {
-  "Momondo scraper should" in {
-    val res = MomondoScraper.scrape(params).get
-    res.currency should be ("EUR")
-    res.url should startWith ("http://www.momondo.com/flightsearch")
+class MomondoSpec extends WordSpecLike with Matchers with TestParams with BeforeAndAfterAll {
+  "Momondo scraper" should {
+    "find cheapest flight" in {
+      val res = MomondoScraper.scrape(params).get
+      res.currency should be("EUR")
+      res.url should startWith("http://www.momondo.com/flightsearch")
+    }
+  }
+
+  override def afterAll(): Unit = {
+    MomondoScraper.webDriver.quit()
   }
 }
